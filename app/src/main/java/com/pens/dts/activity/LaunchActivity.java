a1437aad0f904dc3e6dbfaaafde6f1cc52de35c1
abdaziz-04 <abdaziz1713@gmail.com>
2023-07-09 21:47:39 +0700

    First Commit

---
+++ app/src/main/java/com/pens/dts/activity/LaunchActivity.java
@@ -0,0 +1,16 @@
+package com.pens.dts.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pens.dts.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
